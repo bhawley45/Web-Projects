d73f1603abc8fdb9ee7a7ff1fa64b3adead0bc5a
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-08-20 02:29:31 -0500

    Added new images for testing, organized files, etc
    renamed and organized files to fit a more defined structure (style.css is the main stylesheet, then every major element has an appropriately named sheet to go with it). Added an about and portfolio section. Added new functionality to navbar for fading when the user scrolls

--- Portfolio_Website/js/script.js
+++ Portfolio_Website/js/script.js
@@ -1,6 +1,62 @@
+//=-=-=-=-=-=-=-= Navigation bar drop down menu =-=-=-=-=-=-=-=
+
 const navbarToggle = document.querySelector(".navbar-toggle");
 const navbarLinks = document.querySelector(".navbar-links");
 
 navbarToggle.addEventListener("click", () => {
     navbarLinks.classList.toggle("active");
+});
+
+
+//=-=-=-=-=-=-=-= Portfolio dynamic scrolling =-=-=-=-=-=-=-=
+
+let scrollAmount = 0;
+const projectWidth = 340; // Adjust this width as needed
+const projectsContainer = document.querySelector('.projects-container');
+const projects = document.querySelectorAll('.project');
+
+function scrollProjects(direction) {
+    if (direction === 'left') {
+        scrollAmount -= projectWidth;
+        if (scrollAmount < 0) {
+            scrollAmount = 0;
+        }
+    } else {
+        scrollAmount += projectWidth;
+        const maxScroll = (projects.length - 1) * projectWidth;
+        if (scrollAmount > maxScroll) {
+            scrollAmount = maxScroll;
+        }
+    }
+
+    projectsContainer.style.transform = `translateX(-${scrollAmount}px)`;
+
+    updateButtonStates();
+}
+
+function updateButtonStates() {
+    const leftButton = document.querySelector('.nav-button.left');
+    const rightButton = document.querySelector('.nav-button.right');
+
+    leftButton.disabled = scrollAmount === 0;
+    rightButton.disabled = scrollAmount === (projects.length - 1) * projectWidth;
+}
+
+updateButtonStates();
+
+
+//=-=-=-=-=-=-=-= Navigation bar retreat on scroll =-=-=-=-=-=-=-=
+
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener("scroll", () => {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        document.querySelector(".navbar").classList.remove("scrolled");
+    } else {
+        document.querySelector(".navbar").classList.add("scrolled");
+    }
+
+    prevScrollPos = currentScrollPos;
 });
